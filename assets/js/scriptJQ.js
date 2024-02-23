68b2e28449f0a03513256aa405de1064c9c69d7b
jurgen <jeberoizac@gmail.com>
2024-02-22 21:51:55 -0300

    Pagina de Ricomida
    Pagina de Comidas y sus Recetas

---
+++ assets/js/scriptJQ.js
@@ -0,0 +1,19 @@
+//click para enviar correo
+$(document).ready(function(){
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...")
+    });
+    
+});
+
+//Cambiar color de encabezados h3
+$(document).ready(function(){
+  $("h3").on({
+    
+    dblclick: function(){
+      $(this).css("color", "red");
+    }  
+  });
+});
+
+//esconder parrafos de card card-title
